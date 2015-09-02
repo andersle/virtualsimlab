53995912adbedbe3c9b258cb258c570d1e5f99e0
Anders Lervik <andersle@gmail.com>
2015-09-02 17:34:21 +0200

    Added a very important script

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""This is just an example for the virtual simulation lab."""
+from __future__ import print_function
+
+
+values = [i**2 for i in range(10)]
+
+values.append(10 * 10)
+
+for val in values:
+    print(val)
